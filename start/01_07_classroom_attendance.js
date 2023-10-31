021b0cfd36cb69711d827e30b8d634fba6b3d40b
Vidoje <vidoje.sevic.275.19@ict.edu.rs>
2023-11-01 00:24:33 +0100

    ...closures

--- start/01_07_classroom_attendance.js
+++ start/01_07_classroom_attendance.js
@@ -1,1 +1,18 @@
-// Write your code here
+// Destructuring
+function getStudents(classroom) {
+    let { hasTeachingAssistant, classList } = classroom;
+    let teacher, teachingAssistant, students;
+
+    if (hasTeachingAssistant) {
+        [teacher, teachingAssistant, ...students] = classList;
+    } else {
+        [teacher, ...students] = classList;
+    }
+    return students;
+}
+
+console.log(getStudents({
+    hasTeachingAssistant: false,
+    // hasTeachingAssistant: true,
+    classList: ['Rashida', 'John', 'Roman', 'Lisa', 'Maria', 'Omar', 'Mika', 'Zika']
+}));
